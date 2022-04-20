233d9e06f1ff5f726b4219f15253965f1e6c98c1
Edwin Taborda <eataborda.m@gmail.com>
2022-04-20 16:32:04 -0500

    First commit

---
+++ src/test/java/com/github/eataborda/api/features/exampleapi/ExampleTest.java
@@ -0,0 +1,17 @@
+package com.github.eataborda.api.features.exampleapi;
+
+import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
+import net.thucydides.core.annotations.WithTagValuesOf;
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+@RunWith(SerenityParameterizedRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@WithTagValuesOf({"tag1", "tag2", "tag3"})
+
+public class ExampleTest {
+    private static String value1;
+    private static String value2;
+
+}
