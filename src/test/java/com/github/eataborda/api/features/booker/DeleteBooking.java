426bdbeabff10439bae1721293ac930d3e5536cc
Edwin Taborda <eataborda.m@gmail.com>
2023-12-06 07:55:47 -0500

    Update test to trigger actions

--- src/test/java/com/github/eataborda/api/features/booker/DeleteBooking.java
+++ src/test/java/com/github/eataborda/api/features/booker/DeleteBooking.java
@@ -47,5 +47,4 @@ public class DeleteBooking {
         apiSteps.validateResponseHeadersAreNotNullAndNotEmpty(responseAfterDeletingBooking);
         apiSteps.validateResponseHeadersHasExpectedFields(responseAfterDeletingBooking);
     }
-
 }
